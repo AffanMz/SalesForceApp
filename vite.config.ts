5820431b2870e5910dff5ead0f6a475a0c950fc5
AffanMz <affansukses99@gmail.com>
2026-06-24 07:17:17 +0700

    fix github pages deploy permission

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: './',
+  base: '/SalesForceApp/', // ganti sesuai nama repo
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
